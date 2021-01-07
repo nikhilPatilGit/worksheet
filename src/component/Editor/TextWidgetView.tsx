c134ea721c55954c49774d7888690b99cfc5677a
Nikhil <patilnikhil9@gmail.com>
2021-01-07 08:01:02 +0000

    refactoring done

--- src/component/Editor/TextWidgetView.tsx
+++ src/component/Editor/TextWidgetView.tsx
@@ -1,10 +1,7 @@
 import { Input } from "@chakra-ui/react";
 import React, { FC, useContext, useEffect, useState } from "react";
-import { createActionResult } from "src/helper/Factories";
 import { DocumentReducerContext } from "src/hooks/DocumentProvider";
 import { DocumentAction } from "src/hooks/DocumentProvider/Action";
-import { ActionType } from "src/hooks/DocumentProvider/Types";
-import { Position, UpdateWidgetPosition } from "src/modals/Widget";
 import { TextWidget } from "../Widget/Model";
 
 interface IProps {
@@ -16,14 +13,20 @@ interface IProps {
 const TextWidgetView: FC<IProps> = ({ ...props }) => {
   let offsetX, offsetY;
 
+  const [mouseStatus, setMouseStatus] = useState({
+    isMouseDown: false,
+    isMouseUp: false,
+  });
+
   const move = (e) => {
     const el = e.target;
-    // console.log(`${e.pageX-offsetX}px`+" "+`${e.pageY-offsetY}px`);
-    const left = props.position.x - 25;
-    const top = props.position.y - 25;
+    const left = props.position.x - 100;
+    const top = props.position.y - 20;
 
-    el.style.left = `${left}px`;
-    el.style.top = `${top}px`;
+    if(mouseStatus.isMouseDown){
+      el.style.left = `${left}px`;
+      el.style.top = `${top}px`;
+    }
   };
   const add = (e) => {
     const el = e.target;
@@ -31,41 +34,40 @@ const TextWidgetView: FC<IProps> = ({ ...props }) => {
     offsetY = e.clientY - el.getBoundingClientRect().top;
     el.addEventListener("mousemove", move);
   };
-  const remove = (e) => {
-    const el = e.target;
-    el.removeEventListener("mousemove", move);
-  };
 
-  const [mdStatus, setMdStatus] = useState(false);
+  const [] = useState(false);
   const [cursorType, setCursorType] = useState("text");
 
-  const dispatch: React.Dispatch<DocumentAction> = useContext(
-    DocumentReducerContext
-  );
 
   useEffect(() => {
-      // dispatch(createActionResult<UpdateWidgetPosition>(ActionType.UpdatePosition, new UpdateWidgetPosition(props.widget.widgetId, new Position(props.position.x, props.position.y))));
+    // dispatch(createActionResult<UpdateWidgetPosition>(ActionType.UpdatePosition, new UpdateWidgetPosition(props.widget.widgetId, new Position(props.position.x, props.position.y))));
   }, [props.position.x, props.position.y]);
 
   useEffect(() => {
+    console.log(mouseStatus);
+
     if (!props.isActive) {
-      setMdStatus(false);
+      setMouseStatus({ isMouseDown: false, isMouseUp: false });
     }
   }, [props.isActive]);
 
   return (
     <Input
-      onMouseDown={() => setMdStatus(!mdStatus)}
-      onMouseUp={() => setMdStatus(!mdStatus)}
-      onMouseMove={mdStatus && props.isActive ? add : remove}
+      onMouseDown={() => {
+        setMouseStatus({ isMouseDown: true, isMouseUp: false });
+      }}
+      onMouseUp={() => {
+        setMouseStatus({ isMouseDown: false, isMouseUp: true });       
+      }}
+      onMouseMove={add}
       w="200px"
       variant="filled"
       placeholder="Enter Input"
-      onClick={()=>setCursorType("text")}
-      onMouseEnter={()=>{
+      onClick={() => setCursorType("text")}
+      onMouseEnter={() => {
         setCursorType("all-scroll");
-    }}
-      cursor={cursorType}      
+      }}
+      cursor={cursorType}
     />
   );
 };
